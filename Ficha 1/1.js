b47a81c0d852f5ee684d3679f9dac90316f4e43b
Vict0s <131537860+Vict0s@users.noreply.github.com>
2025-02-04 20:00:02 +0000

    https://github.com/Vict0s/front-end.git

---
+++ Ficha 1/1.js
@@ -0,0 +1,21 @@
+var p1= 30;
+var p2= 40;
+var freq= 30;
+
+function calculategrade (p1,p2,freq){
+    var notaF = p1 * 0.3 + p2 * 0.4 + + freq *0.3; 
+
+console.log("A nota final é:" + notaF)
+
+if(notaF >=9.5)
+    console.log("Aluno reprovado");
+elsec
+    console.log("Aluno reprovado");
+}
+
+calculategrade (17,5,13);
+calculategrade (12,8,10);
+calculategrade (1,15,13);
+calculategrade (7,5,13);l
+calculategrade (19,15,12);
+
