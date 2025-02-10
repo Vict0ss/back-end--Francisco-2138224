b21c88d6e34f99e50e01e66474881c18f94ce062
Vict0s <131537860+Vict0s@users.noreply.github.com>
2025-02-10 10:57:49 +0000

    a

--- Ficha 1/2.js
+++ Ficha 1/2.js
@@ -1,8 +1,11 @@
-
-function calculategrade  (valor1,valor2,op)
+var valor1= 2;
+var valor2= 2;
+function calculategrade  (valor1,valor2,op){
 
     if (op == "+")
-
+    
     return valor1 + valor2 ;
 
     else if (  ); 
+
+}
---
+++ Ficha 1/3.js
@@ -0,0 +1,7 @@
+""" devolve a soma dos primeiros N numeris inteiros, 5   1+2+3+4+5= """
+
+function sum (val)
+    var res=1;
+
+    for(var i=2 ; i =)
+
