258512931af7be489e066f605fa278896e3f93af
Vict0s <131537860+Vict0s@users.noreply.github.com>
2025-02-11 19:49:39 +0000

    a

--- Ficha 2/5.js
+++ Ficha 2/5.js
@@ -1,2 +1,10 @@
 /// escrever uma funçao que desenhe um rentagul com asteriticos
-/// a alrgura  e altura deve, er passadas em argumentos
+/// a alrgura  e altura deve, er passadas como argumentos largura 20 altur 10
+function drawretangle(width,height){
+var line="";
+    for(let i=0; i wight; i++){
+        line="*";
+        console.log(line);
+    }
+}
+console.long(10,10)
