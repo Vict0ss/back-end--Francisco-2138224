365ae0dc7906022a22fabc6238627e37f2da96ae
Vict0s <131537860+Vict0s@users.noreply.github.com>
2025-02-17 11:24:23 +0000

    a

--- Ficha 3/app.js
+++ Ficha 3/app.js
@@ -1,7 +1,19 @@
 ///<> {}
-function performDownload (started, update,completed) 
 
+function started ()  {
+    console.log("started Download")
+}
+function update ()  {
+    console.log("Download + vue+ 1%")
+}
+function completed ()  {
+    console.log("completed Download")
+}
+function performDownload (started, update,completed) ;
+    started();
+    for(let i = 0; i <= 100; i++){
+        update
+    }
 
-
-console.log("started")
-console.log("completed")
+    var arrayutils = requiere('./arrayUtils.js');
+    arrayutils.isEmmpty
---
+++ Ficha 3/arrayUtils.js
@@ -0,0 +1,8 @@
+var arrayutils={
+isEmpty: function(array){
+    return array.length ==0;
+}
+};
+
+module.exports = arrayutils
+
