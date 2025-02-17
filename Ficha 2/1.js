d82e7af4031a5268df6fe103c3851bd72a21a568
Vict0s <131537860+Vict0s@users.noreply.github.com>
2025-02-17 10:57:06 +0000

    a

--- Ficha 2/1.js
+++ Ficha 2/1.js
@@ -3,16 +3,13 @@
 /// acima 25 a 30    obeso   acima 30
 /// imc= peso/altura2
 
-// Função para calcular o índice de massa corporal (IMC)
-var peso = 80; // em kg
-var altura = 1.20; // em metros (120 cm = 1.20 m)
+var peso = 80; 
+var altura = 1.20;
 
-// Calculando o IMC
-var imc = peso / Math.pow(altura, 2); // fórmula correta
+var imc = peso / Math.pow(altura, 2); 
 
 console.log("O índice de massa corporal é:", imc.toFixed(2));
 
-// Verificando a faixa de peso com base no IMC
 if (imc < 18.5) {
     console.log("Abaixo do peso");
 } else if (imc >= 18.5 && imc < 25) {
--- Ficha 3/app.js
+++ Ficha 3/app.js
@@ -1,1 +1,7 @@
 ///<> {}
+function performDownload (started, update,completed) 
+
+
+
+console.log("started")
+console.log("completed")
