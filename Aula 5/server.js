c22fb0fe65c8cdd72c928d5abcba77c4f59746aa
Vict0s <131537860+Vict0s@users.noreply.github.com>
2025-03-03 10:54:50 +0000

    a

--- Aula 5/server.js
+++ Aula 5/server.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/users', (req, res) => {
+  res.send('dataobj')
 })
 
+app.use(express.json());
+app.post('/users', (req, res) => {
+    var newPerson = req.body;
+    res.send(req.body)
+  })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- Ficha 4/app.js
+++ Ficha 4/app.js
@@ -1,7 +1,34 @@
 ///<> {}
-var name = Object.create(nome);
+    var name = Object.create(nome);
 john.firstname = 'John';
 john.lastname = 'Doe';
 var age = Object.create(idade);
 
 var gender = Object.create(genero);
+
+
+
+
+
+
+
+const express = require('express')
+const fs = require(fs);
+const app = express()
+const port = 3000
+
+var fileContent = fs.readFileSync("persons.json")
+var dataobj = Json.parse(fileContent);
+
+
+app.get('/users', (req, res) => {
+  res.send('dataobj')
+})
+
+app.post('/users', (req, res) => {
+    var newPerson = req.body;
+    res.send(req.body)
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
