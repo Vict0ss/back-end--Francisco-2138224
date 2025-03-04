66d6a486dd0161962c5da03571cbc7f8e0d8f861
Vict0ss <franciscoalvespt2019@gmail.com>
2025-03-04 21:18:41 +0000

    b

--- Aula 5/server.js
+++ Aula 5/server.js
@@ -2,15 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.get('/users', (req, res) => {
   res.send('dataobj')
 })
 
+
+
 app.use(express.json());
 app.post('/users', (req, res) => {
     var newPerson = req.body;
-    res.send(req.body)
+    dataObj.data.push(newPerson)
+    res.send("new person was added")
   })
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
