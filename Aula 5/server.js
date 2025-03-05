e69e44de63dffbf54fca009bd9d5fe1fab463fff
Vict0ss <franciscoalvespt2019@gmail.com>
2025-03-05 19:48:07 +0000

    a

--- Aula 5/server.js
+++ Aula 5/server.js
@@ -8,14 +8,16 @@ var dataobj = JSON.parse(fileContent); // convertendo arquivo pra json
 console.log  ("porque existe codigo",fileContent) // testando so pra saber se da
 
 app.get('/users', (req, res) => { // abrindo o  get
-  res.send('dataobj')
+  res.send(JSON.parse(fileContent) ) 
 })
 
 app.use(express.json()); // aceitar conteudos em json
 app.post('/users', (req, res) => { // abrindo novamente mas aceitar dados 
     var newPerson = req.body; // pegando dados 
+    var dataobj = JSON.parse(fileContent);  
     console.log  ("socorro deus",newPerson) // convertendo pra json e adicionado no .json
-    fs.writeFileSync('persons.json', JSON.stringify(newPerson));
+    dataobj.data.push(newPerson); 
+    fs.writeFileSync('persons.json',JSON.stringify(dataobj))
     res.send("new person was added")   // resposta que mandou a ppessoa
   })
 
