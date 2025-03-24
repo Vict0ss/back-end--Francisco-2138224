4d3f6bbb95e5aeb60dd53ca6bff926d676b1aa15
Vict0ss <franciscoalvespt2019@gmail.com>
2025-03-24 09:51:23 +0000

    a

--- Aula 5/cliente.js
+++ Aula 5/cliente.js
@@ -1,12 +1,15 @@
-import axios from 'axios';
+//import axios from 'axios';
 const axios = require('axios'); // legacy way
 
+
 // Make a request for a user with a given ID
-axios.get('/user?ID=12345')
+axios.get('http://localhost:3000/test')
   .then(function (response) {
     // handle success
     console.log(response);
   })
+
+
   .catch(function (error) {
     // handle error
     console.log(error);
